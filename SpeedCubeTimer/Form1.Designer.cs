42dd2492634833429a4998daa5d7cb24f938b55c
Dyoza <Dyoza@DESKTOP-NP1ISP5>
2017-01-27 20:25:36 -0800

    Add project files.

---
+++ SpeedCubeTimer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SpeedCubeTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmrUpdatesPerSecond = new System.Windows.Forms.Timer(this.components);
+            this.txtScramble = new System.Windows.Forms.TextBox();
+            this.lstBoxScores = new System.Windows.Forms.ListBox();
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Bebas Neue Bold", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 282);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(647, 87);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tmrUpdatesPerSecond
+            // 
+            this.tmrUpdatesPerSecond.Interval = 1;
+            this.tmrUpdatesPerSecond.Tick += new System.EventHandler(this.tmrUpdatesPerSecond_Tick);
+            // 
+            // txtScramble
+            // 
+            this.txtScramble.BackColor = System.Drawing.SystemColors.Control;
+            this.txtScramble.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtScramble.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtScramble.Location = new System.Drawing.Point(12, 23);
+            this.txtScramble.Name = "txtScramble";
+            this.txtScramble.ReadOnly = true;
+            this.txtScramble.Size = new System.Drawing.Size(647, 33);
+            this.txtScramble.TabIndex = 4;
+            this.txtScramble.TabStop = false;
+            this.txtScramble.Text = "SCRAMBLE";
+            this.txtScramble.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lstBoxScores
+            // 
+            this.lstBoxScores.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBoxScores.FormattingEnabled = true;
+            this.lstBoxScores.HorizontalScrollbar = true;
+            this.lstBoxScores.ItemHeight = 16;
+            this.lstBoxScores.Location = new System.Drawing.Point(665, 23);
+            this.lstBoxScores.Name = "lstBoxScores";
+            this.lstBoxScores.Size = new System.Drawing.Size(484, 260);
+            this.lstBoxScores.TabIndex = 5;
+            this.lstBoxScores.TabStop = false;
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTimer.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtTimer.Font = new System.Drawing.Font("Bebas Neue Bold", 128.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimer.Location = new System.Drawing.Point(12, 51);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.ReadOnly = true;
+            this.txtTimer.Size = new System.Drawing.Size(647, 206);
+            this.txtTimer.TabIndex = 6;
+            this.txtTimer.TabStop = false;
+            this.txtTimer.Text = "00:00:00:00";
+            this.txtTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(666, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Best:";
+            // 
+            // txtBest
+            // 
+            this.txtBest.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBest.Location = new System.Drawing.Point(716, 287);
+            this.txtBest.Name = "txtBest";
+            this.txtBest.ReadOnly = true;
+            this.txtBest.Size = new System.Drawing.Size(433, 22);
+            this.txtBest.TabIndex = 8;
+            this.txtBest.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 381);
+            this.Controls.Add(this.txtBest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTimer);
+            this.Controls.Add(this.lstBoxScores);
+            this.Controls.Add(this.txtScramble);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Bebas Neue Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Speed Cube Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tmrUpdatesPerSecond;
+        private System.Windows.Forms.TextBox txtScramble;
+        private System.Windows.Forms.ListBox lstBoxScores;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBest;
+    }
+}
+
---
+++ SpeedCubeTimer/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SpeedCubeTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //initialize variables
+        String scramble;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrUpdatesPerSecond.Start();
+            scramble = Scrambler();
+            txtScramble.Text = scramble;
+            
+        }
+
+        //initialize more variables
+        Stopwatch stpTimer = new Stopwatch();
+        bool blTimer = false;
+        int intSolves = 0;
+        TimeSpan tsTimer;
+        List<string> lstScrambles = new List<string>();
+        List<TimeSpan> lstTimes = new List<TimeSpan>();
+        String Best = "100000000";
+        
+        
+
+        //click button
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartStop();
+        }
+
+
+        //updated timer
+        private void tmrUpdatesPerSecond_Tick(object sender, EventArgs e)
+        {
+            tsTimer = stpTimer.Elapsed;
+            txtTimer.Text = TimeFormatter(tsTimer);
+        }
+
+        //scrambler
+        private string Scrambler()
+        {
+            String scramble = "";
+            Random rnd = new Random();
+            int number;
+            int prevNumber = 0;
+            String letter = "";
+            String[] NormalLetters = new String[12] {"R", "R'", "L", "L'", "F", "F'", "B", "B'", "U", "U'", "D", "D'", };
+            String[] DoubleLetters = new String[12] { "2R", "2R", "2L", "2L", "2F", "2F", "2B", "2B", "2U", "2U", "2D", "2D", };
+
+            for (int i = 0; i < 20; ++i)
+            {
+
+                number = rnd.Next(1, 12);
+
+                if (prevNumber != number)
+                {
+                    letter = NormalLetters[number];
+                    prevNumber = number;
+                }
+                else
+                {
+                    letter = DoubleLetters[number];
+                    prevNumber = 0;
+                    ++i;
+                }
+
+                scramble += "  " + letter;
+            }
+            return scramble;
+        }
+
+        //time formatter
+        private string TimeFormatter(TimeSpan ts)
+        {
+            string Timer = "";
+
+            if (stpTimer.ElapsedMilliseconds > 3.6e+6)
+            {
+                Timer = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            }
+            else if (stpTimer.ElapsedMilliseconds > 60000)
+            {
+                Timer = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            }
+            else
+            {
+                Timer = String.Format("{0:00}:{1:00}", ts.Seconds, ts.Milliseconds / 10);
+            }
+
+            return Timer;
+        }
+
+
+        //Start/stop
+        private void StartStop()
+        {
+            //if it's stopped, start it. if it's started, stop it
+            if (blTimer == false)
+            {
+                blTimer = true;
+                btnStart.Text = "Stop";
+                stpTimer.Restart();
+
+                lstScrambles.Add(scramble);
+
+                scramble = Scrambler();
+                txtScramble.Text = scramble;
+
+
+            }
+            else
+            {
+                blTimer = false;
+                btnStart.Text = "Start";
+                stpTimer.Stop();
+                lstTimes.Add(tsTimer);
+                lstBoxScores.Items.Insert(0, intSolves + 1 + ".   " + TimeFormatter(tsTimer) + "   " + lstScrambles[intSolves]);
+
+
+                if (stpTimer.ElapsedMilliseconds < Convert.ToInt32(Best))
+                {
+                    Best = Convert.ToString(stpTimer.ElapsedMilliseconds);
+                    txtBest.Text = TimeFormatter(tsTimer) + "   " + lstScrambles[intSolves];
+                }
+
+                intSolves++;
+            }
+        }
+
+  
+    }
+}
